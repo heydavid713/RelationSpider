aefcbc900f4b514e2d977fe4ded8d332c7cd6713
heydavid713 <ddineroxx@gmail.com>
2016-09-08 15:35:45 -0400

    Update RelationSpider.php

--- src/Heydavid713/RelationSpider/RelationSpider.php
+++ src/Heydavid713/RelationSpider/RelationSpider.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
-use Illuminate\Database\Eloquent\Relations\MorphTo;z
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 trait RelationSpider
 {
